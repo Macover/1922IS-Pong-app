7c18bb19f2e84f9fe7b137d04ab3c2431ca837ee
Macover <macover1507@hotmail.com>
2022-05-24 23:53:34 -0500

    metodo velocidades en Pelota class

--- app/src/main/java/com/example/pong/Pelota.java
+++ app/src/main/java/com/example/pong/Pelota.java
@@ -2,23 +2,65 @@ package com.example.pong;
 
 import android.graphics.RectF;
 
+import java.util.Random;
+
 public class Pelota {
 
-    private RectF cordenadasRectangulo;
-    private float velocidadX;
-    private float velocidadY;
+    private RectF mCoordenadasRec;
+    private float mVelcidadX;
+    private float mVelicidadY;
     private float pelotaAncho;
     private float pelotaAlto;
 
-    public Pelota(int pantallaX, int pantallaY){
-        //La pelota es una centésima parte del ancho de la pantalla.
-        this.pelotaAncho = pantallaX / 100;
-        this.pelotaAlto = this.pelotaAncho;
-        /* La pelota va a 1/4 de altura de la pantalla
-        cada segundo.*/
-        this.velocidadX = this.velocidadY;
-        this.velocidadY = pantallaY / 4;
-        
+    public Pelota(int screenX, int screenY){
+        pelotaAncho = screenX / 100;
+        pelotaAlto = pelotaAncho;
+        mVelicidadY = screenY / 4;
+        mVelcidadX = mVelicidadY;
+        mCoordenadasRec = new RectF();
+    }
+    public RectF getRect(){
+        return mCoordenadasRec;
+    }
+    public void actualizar(long fps){
+        mCoordenadasRec.left = mCoordenadasRec.left + (mVelcidadX / fps);
+        mCoordenadasRec.top = mCoordenadasRec.top + (mVelicidadY / fps);
+        mCoordenadasRec.right = mCoordenadasRec.left + pelotaAncho;
+        mCoordenadasRec.bottom = mCoordenadasRec.top - pelotaAlto;
+    }
+    public void invertirVelocidadX(){
+        mVelicidadY = -mVelicidadY;
     }
 
+    public void invertirVelocidadY(){
+        mVelcidadX = -mVelcidadX;
+    }
+
+    public void setRandomVelocidad(){
+        Random generator = new Random();
+        int answer = generator.nextInt(2);
+        if(answer == 0){
+            invertirVelocidadX();
+        }
+    }
+    public void aumentarVelocidad(){
+        mVelcidadX = mVelcidadX + mVelcidadX / 10;
+        mVelicidadY = mVelicidadY + mVelicidadY / 10;
+    }
+    public void quitarObsY(float y){
+        mCoordenadasRec.bottom = y;
+        mCoordenadasRec.top = y - pelotaAlto;
+    }
+
+    public void quitarObsX(float x){
+        mCoordenadasRec.left = x;
+        mCoordenadasRec.right = x + pelotaAncho;
+    }
+
+    public void reset(int x, int y){
+        mCoordenadasRec.left = x / 2;
+        mCoordenadasRec.top = y - 20;
+        mCoordenadasRec.right = x / 2 + pelotaAncho;
+        mCoordenadasRec.bottom = y - 20 - pelotaAlto;
+    }
 }
